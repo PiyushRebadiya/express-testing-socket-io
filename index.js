0b5e127202164d7e6361e8a040a1820ae5bf36a0
Piyush Rebadiya <piyushrebadiya123@gmail.com>
2024-04-13 10:26:36 +0530

    done whatsup web task

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const axios = require('axios');
 const moment = require('moment');
 const bodyParser = require('body-parser');
-const { LocalAuth, MessageMedia, Client } = require('whatsapp-web.js');
+const { LocalAuth, Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const qrimage = require('qr-image');
-const fs = require('fs');
 
 // const wwebVersion = '2.2407.3';
 
@@ -17,20 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
-
-async function urlToBase64(url) {
-  try {
-    const response = await axios.get(url, {
-      responseType: 'arraybuffer'
-    });
-    const base64Data = Buffer.from(response.data, 'binary').toString('base64');
-    console.log("Successfully converted URL to base64!");
-    return base64Data;
-  } catch (error) {
-    console.error('Error fetching image:', error);
-    throw error;
-  }
-}
 
 let qrCodeData = null;
 let whatsupConnect = null;
@@ -171,7 +154,7 @@ app.get("/whatsapp", async (req, res) => {
         console.log("sendMessage", r)
       })
     }
-    res.send({
+    await res.send({
       status: "true",
       message: "Message sent successfully!"
     });
@@ -185,70 +168,6 @@ app.get("/whatsapp", async (req, res) => {
     res.status(500).send(error?.message || 'Something went wrong');
   }
 })
-
-
-// Function to make the API call
-const callApi = async () => {
-  const todayDate = moment().format('YYYY-MM-DD')
-  try {
-    await axios.get(`https://report.taxfile.co.in/Report/TaskSummaryReportCustom?CompanyID=267&ReportMode=EXPORT&custid=taxfilecrm&PartyId=0&TaskStatus=&startdate=${todayDate}&endDate=${todayDate}&AssignTo=&AssignBy=&ProjectId=0&CategoryId=0&TaxadminId=0&TaskType=Task&ToMail=shyamkhokhariya97@gmail.com&Subject=Task Report ${todayDate}`, {
-      mode: 'no-cors'
-    });
-    console.log(`Report sent to successfully: ${new Date().toLocaleString()}`);
-  } catch (error) {
-    console.error('Error calling API:', error.message);
-  }
-};
-// Calculate the time until the next 6:30 PM
-const calculateNextCallTime = () => {
-  const now = new Date();
-  const nextCallTime = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    18, // 6:30 PM
-    30,
-    0
-  );
-  if (nextCallTime <= now) {
-    nextCallTime.setDate(nextCallTime.getDate() + 1); // Next day
-  }
-  return nextCallTime.getTime() - now.getTime();
-};
-
-// // Call the API initially
-// callApi();
-
-// // Set interval to call the API daily at 6:30 PM
-// const interval = calculateNextCallTime();
-// setTimeout(() => {
-//   callApi();
-//   setInterval(callApi, 24 * 60 * 60 * 1000); // Repeat every 24 hours
-// }, interval);
-
-app.post("/base64", async function (req, res) {
-  try {
-    if (!req?.body || !req?.body?.url) {
-      return res.status(400).json({ error: 'URL is missing in the request body' });
-    }
-    const url = req?.body?.url;
-    const imageExtension = url?.split('.')?.pop();
-    const base64Data = await urlToBase64(url);
-    res.json({
-      data: `data:image/${imageExtension};base64,${base64Data}`,
-      success: true
-    });
-  } catch (error) {
-    let errorMessage = 'Internal Server Error';
-    if (error.response && error.response.status === 404) {
-      errorMessage = 'Data not found';
-    } else if (error.response && error.response.status === 403) {
-      errorMessage = 'Forbidden: Access denied to the image';
-    }  // Add more specific error handling as needed
-    console.error('Error:', error);
-    res.status(error?.response?.status ||500).json({ error: errorMessage });
-  }
-});
 
 server.listen(808, () => {
   console.log("SERVER IS RUNNING 808");
