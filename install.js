7967a599f4900e8f696c9a4c5261155bb873a929
Piyush Rebadiya <piyushrebadiya123@gmail.com>
2024-04-24 10:45:20 +0530

    change to install js file

--- install.js
+++ install.js
@@ -5,6 +5,7 @@ var svc = new Service({
     name: 'Taxfile Web Socket Node CRM Server',
     description: 'Running Web Socket Node CRM Server Of Taxfile',
     script: 'C:\\inetpub\\wwwroot\\WebSocketServer\\socket-node\\index.js',
+    // script: 'C:\\Users\\ARTIS\\Desktop\\express-testing-socket-io\\index.js',
     //, workingDirectory: '...'
     //, allowServiceLogon: true
 });
@@ -25,3 +26,11 @@ svc.install();
 
 // // Uninstall the service.
 // svc.uninstall();
+
+svc.on('error', (error) => {
+  console.error('Service error:', error.message);
+});
+
+svc.on('start', () => {
+  console.log('Service started successfully.');
+});
